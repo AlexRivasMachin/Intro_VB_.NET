33af6f7d102b074dbd270eef7c9029072f0e11b8
AlexSISISI <116727866+AlexSISISI@users.noreply.github.com>
2024-01-23 21:52:22 +0100

    ConsoleCuota

---
+++ ConsoleCuota/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim c As New CuotaAnual.Cuota
+        Console.WriteLine("Inglrese el capital total")
+        Dim cap As Integer = Console.ReadLine
+        Console.WriteLine("Ingrese el plazo")
+        Dim p As Integer = Console.ReadLine
+        Console.WriteLine("Ingrese el interes")
+        Dim i As Integer = Console.ReadLine
+        Console.WriteLine("La comision es: " & c.cuotaAnual(cap, p, i))
+        Console.ReadKey()
+
+    End Sub
+
+End Module
