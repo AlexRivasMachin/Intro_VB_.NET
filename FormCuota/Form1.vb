2aabb8338daabab98ad044671886b70818047525
AlexSISISI <116727866+AlexSISISI@users.noreply.github.com>
2024-01-23 21:47:16 +0100

    FormCuota

---
+++ FormCuota/Form1.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Buttom1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cuota As New CuotaAnual.Cuota
+        TextBox4.Text = cuota.cuotaAnual(CSng(TextBox1.Text), CInt(TextBox2.Text), CInt(TextBox3.Text))
+    End Sub
+End Class
