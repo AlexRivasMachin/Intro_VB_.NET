b369ca0a4847bc8b3eb59c51b56796942069c469
AlexSISISI <116727866+AlexSISISI@users.noreply.github.com>
2024-01-23 19:48:29 +0100

    FUNCIONA

--- FormComisiones/Form1.vb
+++ FormComisiones/Form1.vb
@@ -2,10 +2,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim ventas As New Comisiones__Labo_1___Alex_Rivas.Ventas
 
-        TextBox3.Text = ventas.comision(CSng(TextBox2.Text), CInt(TextBox1.Text))
+        TextBox3.Text = ventas.Comision(CSng(TextBox2.Text), CInt(TextBox1.Text))
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
