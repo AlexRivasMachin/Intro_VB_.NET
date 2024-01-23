1fbc1143387e6e9e6d703d73635a59c88d66fbfc
AlexSISISI <116727866+AlexSISISI@users.noreply.github.com>
2024-01-23 19:17:36 +0100

    First Steps

--- Comisiones (Labo 1)- Alex Rivas/Comision.vb
+++ Comisiones (Labo 1)- Alex Rivas/Comision.vb
@@ -1,7 +1,11 @@
 ﻿Public Class Ventas
     Public Property TotalVentas As Integer = 1000000.0
     Public Property NumeroVentas As Integer = 10000
-    Public Function Comision() As Single
-        Comision = TotalVentas * 0.025 + ((TotalVentas / NumeroVentas) * 0.05)
+    Public Function comision() As Single
+        comision = TotalVentas * 0.025 + ((TotalVentas / NumeroVentas) * 0.05)
     End Function
+    Public Function comision(ByVal TotalVentas As Integer, ByVal NumeroVentas As Integer) As Single
+        comision = TotalVentas * 0.025 + ((TotalVentas / NumeroVentas) * 0.05)
+    End Function
+
 End Class
