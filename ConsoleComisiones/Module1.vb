343ce8ef5eb7c387e688b0fb796c14a0b4bea4a6
AlexSISISI <116727866+AlexSISISI@users.noreply.github.com>
2024-01-23 20:08:02 +0100

    ConsoleApp

---
+++ ConsoleComisiones/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim c As New Comisiones__Labo_1___Alex_Rivas.Ventas
+        Console.WriteLine("Ingrese el total de ventas")
+        Dim tv As Integer = Console.ReadLine
+        Console.WriteLine("Ingrese el numero de ventas")
+        Dim nv As Integer = Console.ReadLine
+        Console.WriteLine("La comision es: " & c.comision(tv, nv))
+        Console.ReadKey()
+
+    End Sub
+
+End Module
