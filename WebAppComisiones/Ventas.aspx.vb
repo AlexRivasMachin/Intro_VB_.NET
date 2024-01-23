e5b9291856bdd1f1af165ecdf549245383f41a57
AlexSISISI <116727866+AlexSISISI@users.noreply.github.com>
2024-01-23 21:18:38 +0100

    Web + Borrar kk

---
+++ WebAppComisiones/Ventas.aspx.vb
@@ -0,0 +1,13 @@
+﻿Public Class Ventas
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Buttom1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim c As New Comisiones__Labo_1___Alex_Rivas.Ventas
+        TextBox3.Text = c.comision(CSng(TextBox2.Text), CInt(TextBox1.Text))
+    End Sub
+
+End Class
