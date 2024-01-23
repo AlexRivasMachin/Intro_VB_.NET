d5185dbe4787c23353c0ef176de1952e5963a6ad
AlexSISISI <116727866+AlexSISISI@users.noreply.github.com>
2024-01-23 19:05:04 +0100

    Agregar archivos de proyecto.

---
+++ Comisiones (Labo 1)- Alex Rivas/Comision.vb
@@ -0,0 +1,7 @@
+﻿Public Class Ventas
+    Public Property TotalVentas As Integer = 1000000.0
+    Public Property NumeroVentas As Integer = 10000
+    Public Function Comision() As Single
+        Comision = TotalVentas * 0.025 + ((TotalVentas / NumeroVentas) * 0.05)
+    End Function
+End Class
